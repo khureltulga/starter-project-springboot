0118d125edddb7d2e47901dceecd012669f65bdf
khureltulga <bkhureltulga@gmail.com>
2016-08-22 14:21:24 +0900

    sc

--- src/main/java/com/tulgaa/TulgaaApplication.java
+++ src/main/java/com/tulgaa/TulgaaApplication.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.datatables.repository.DataTablesRepositoryFactoryBean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,8 +13,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(repositoryFactoryBeanClass = DataTablesRepositoryFactoryBean.class)
 public class TulgaaApplication {
 
+	public static String ROOT = "uploads";
+	
+	@Bean
+	CommandLineRunner init() {
+      return (String[] args) -> {
+          new File(ROOT).mkdir();
+      };
 	}
+	 
 	public static void main(String[] args) {
 		SpringApplication.run(TulgaaApplication.class, args);
 	}
+	
 }
